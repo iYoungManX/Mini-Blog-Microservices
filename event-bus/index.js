c59934449e5d7fd9c03dd9c1f6315c1fe232ba54
iYoungManX <90013928+iYoungManX@users.noreply.github.com>
2022-05-01 03:51:56 -0400

    1 st update

---
+++ event-bus/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+const events = [];
+app.post("/events", (req, res) => {
+  const event = req.body;
+  events.push(event);
+  //posts
+  axios.post("http://localhost:4000/events", event);
+  //comments
+  axios.post("http://localhost:4001/events", event);
+  //query
+  axios.post("http://localhost:4002/events", event);
+  //moderation
+  axios.post("http://localhost:4003/events", event);
+  res.send({ status: "OK" });
+});
+
+app.get('/events',(req,res)=>{
+  res.send(events);
+})
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+
+});
